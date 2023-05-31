1e63e22d6b87527beef75bff41f4d25e14b0779b
Jitu Meher <jitumeher217@gmail.com>
2023-05-31 18:41:08 +0530

    leetcode questions

--- Pepcoding/StackAndQueue/questions.java
+++ Pepcoding/StackAndQueue/questions.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.util.LinkedList;
+import java.util.Arrays;
 public class questions {
 
 
@@ -57,10 +58,84 @@ public class questions {
 		return false;
 	}
 
-
-
     
-    public static void main(String[] args) {
 
+
+
+
+
+
+
+    public static int[] getNextGreaterElements(int[]arr) {
+        LinkedList<Integer> st = new LinkedList<>();
+        int n = arr.length;
+        int[]ans  = new int[n];
+        Arrays.fill(ans, -1);
+
+        for(int i = 0; i < n; i++) {
+            while(st.size() != 0 && arr[st.getFirst()] < arr[i]) 
+                ans[st.removeFirst()] = arr[i];
+            
+            st.push(i);
+        }
+        return ans;
+    }
+
+
+
+
+
+
+
+
+    // merge intervals
+    public static void mergeIntervals(int[][]arr) {
+        Arrays.sort(arr, (a,b) -> {
+            return a[0] - b[0];
+        });
+
+        LinkedList<int[]> st = new LinkedList<>();
+
+        for(int[] a : arr) {
+            int minStartTime = a[0];
+            int maxEndTime = a[1];
+
+            while(st.size() != 0 && a[0] <= st.getFirst()[1]) {
+                minStartTime = st.getFirst()[0];
+                maxEndTime = Math.max(maxEndTime, st.getFirst()[1]);
+                st.removeFirst();
+            }
+            st.addFirst(new int[] {minStartTime, maxEndTime});
+        }
+
+        while(st.size() != 0) {
+            int[]a = st.removeLast();
+            System.out.println(a[0] + " " + a[1]);
+        }
+
+
+
+
+
+
+
+
+
+        // for(int i = 0; i < arr.length; i++) {
+        //     for(int j = 0; j < arr[i].length; j++) {
+        //         System.out.print(arr[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+    }
+    public static void main(String[] args) {
+        // int[] arr = {2,5,9,3,1,12,6,8,7};
+        // int[] ans = getNextGreaterElements(arr);
+
+        // for(int ele : ans)
+        //     System.out.println(ele);
+        int[][]arr = {{1,8}, {5,12},  {14,19}, {22,28}, {25,27},  {27,30}};
+        int[][]arr2 = {{1,3},{2,6},{8,10},{15,18}};
+        mergeIntervals(arr2);
     }
 }
