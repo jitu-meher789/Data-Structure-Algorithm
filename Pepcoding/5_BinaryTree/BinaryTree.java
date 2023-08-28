6f4ddb781c92018c06f2bcc1282822e3f99ea442
Jitu Meher <jitumeher217@gmail.com>
2023-08-28 23:04:32 +0530

    binary Tree continued

--- Pepcoding/5_BinaryTree/BinaryTree.java
+++ Pepcoding/5_BinaryTree/BinaryTree.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BinaryTree {
     public static class Node {
         int data;
@@ -51,9 +53,47 @@ public class BinaryTree {
         }
 
         public static int size(Node node) {
+            if(node == null){
+                return 0;
+            }
 
+            
+            int leftSize =  size(node.left);
+            int rightSize = size(node.right);
+
+            return leftSize + rightSize + 1;
+            
         }
         
-        public staic int 
+        public static int sum(Node node) {
+            if(node == null){
+                return 0;
+            }
+
+            int leftSum = sum(node.left);
+            int rightSum = sum(node.right);
+
+            return leftSum + rightSum + node.data;
+        }
+
+        public static int max(Node node) {
+            if(node == null)
+             return -(int)1e9;
+
+            int leftMax  = max(node.left);
+            int rightMax = max(node.right);
+
+            return Math.max(Math.max(leftMax, rightMax), node.data);
+        }
+
+         public static int min(Node node) {
+            if(node == null)
+             return (int)1e9;
+
+            int leftMin  = min(node.left);
+            int rightMin = min(node.right);
+
+            return Math.min(Math.min(leftMin, rightMin), node.data);
+        }
     }
 }
